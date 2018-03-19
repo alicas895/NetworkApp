bf91ef20d1835282380282b8573ffda9d9ad0728
Alicia <alicia.cascella@uconn.edu>
2018-03-19 15:08:11 -0400

    Test

---
+++ NetworkApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ProductsApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.EnableCors();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{xpro}/{ypro}/{pxy}/{py}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ NetworkApp/Controllers/NetworkController.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace NetworksApp.Controllers
+{
+    public class NetworkController : ApiController
+    {
+        [HttpGet]
+        [EnableCors(origins: "*", headers: "*", methods: "*" )]
+        public IHttpActionResult Add(double xpro, double ypro, double pxy, double py){
+            double sum = xpro + ypro + pxy + py;
+            return Ok(sum);
+        }
+    }
+}
